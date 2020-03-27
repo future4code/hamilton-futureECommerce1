5a860462431418cb08cff4a2b988dcaf277deefd
BroodMaiden <41172576+BroodMaiden@users.noreply.github.com>
2020-03-27 16:52:05 -0300

    Site

--- src/Components/Catalogo.js
+++ src/Components/Catalogo.js
@@ -1,59 +1,82 @@
 import React, { Component } from "react";
 import Produto from "./Produto";
-import styled from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  padding: 0 10px;
-  margin: 10px;
-`;
-const Header = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 10px;
-`;
-
-const ListaDeProdutos = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  column-gap: 20px;
-  row-gap: 20px;
-`;
-
-let crescente = false;
+import styled, { css } from "styled-components";
+import { isElementOfType } from "react-dom/test-utils";
 
 export default class Catalogo extends Component {
+  state = {
+    crescente: "true"
+  };
 
+  mudarSeletor = event => {
+    this.setState({
+      crescente: event.target.value
+    });
+  };
 
   render() {
     const listaCrescente = this.props.propsListaDeProdutos.sort(
       (a, b) => a.preco - b.preco
     );
 
-    if (!crescente) listaCrescente.reverse();
-
+    if (this.state.crescente === "false") listaCrescente.reverse();
     return (
-      <Container>
+      <Container sticky={this.props.sticky}>
         <Header>
           <p>Quantidade de Produtos: {listaCrescente.length}</p>
-          <select>
-            <option>Preço: Crescente</option>
-            <option>Preço: Decrescente</option>
+          <select value={this.state.crescente} onChange={this.mudarSeletor}>
+            <option value="true">Preço: Crescente</option>
+            <option value="false">Preço: Decrescente</option>
           </select>
         </Header>
         <ListaDeProdutos>
           {listaCrescente.map(element => (
-            <Produto 
-            adicionaAoCarrinho={this.props.propsFuncaoAdicionar}
-            propsViagem={element.nome, element.preco, element.quantidade} 
-            propsNome={element.nome} 
-            propsPreco={element.preco} />
+            <Produto
+              adicionaAoCarrinho={this.props.propsFuncaoAdicionar}
+              propsNome={element.nome}
+              propsPreco={element.preco}
+              propsImagem={element.img}
+              key={element.id}
+            />
           ))}
         </ListaDeProdutos>
       </Container>
     );
   }
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  box-shadow: inset 0 0 10px #000000;
+  flex: 1;
+
+  ${props =>
+    props.sticky &&
+    css`
+      padding-top: 370px;
+    `}
+`;
+const Header = styled.div`
+  margin-top: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  width: 90%;
+`;
+
+const ListaDeProdutos = styled.div`
+  padding: 10px;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: 20px;
+  row-gap: 20px;
+`;
